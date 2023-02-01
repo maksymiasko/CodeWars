11b3868e241c50a3c3d7017a1c3035a30a471da8
Aleksandr Akimov <gtwood@yandex.ru>
2023-02-01 21:13:29 +0300

    added titleÐ

--- src/main/java/alexandrChg/BeginnerSeries2Clock.java
+++ src/main/java/alexandrChg/BeginnerSeries2Clock.java
@@ -1,5 +1,13 @@
 package alexandrChg;
 
+    /*
+     * Beginner Series #2 Clock
+     * https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/java
+     *
+     * Clock shows h hours, m minutes and s seconds after midnight.
+     * Your task is to write a function which returns the time since midnight in milliseconds.
+     */
+
 public class BeginnerSeries2Clock {
 
     public static int Past(int h, int m, int s) {
