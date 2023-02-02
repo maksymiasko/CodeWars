15107a119f3f91a4ff0a36c0506a4dacb452e2ff
verafes <verafes@gmail.com>
2023-02-01 18:40:34 -0800

    QuarterOfTheYear vera

--- src/main/java/verafes/StringyStrings.java
+++ src/main/java/verafes/StringyStrings.java
@@ -1,6 +1,7 @@
 package verafes;
 
 /* Stringy Strings
+* https://www.codewars.com/kata/563b74ddd19a3ad462000054
 * write me a function stringy that takes a size and returns a string of alternating '1s' and '0s'.
 * the string should start with a 1.
 * a string with size 6 should return :'101010'.
