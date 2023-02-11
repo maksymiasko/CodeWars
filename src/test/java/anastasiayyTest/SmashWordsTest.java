07154aba503220791d3fbfd04c10dad94769f250
Anastasia Yakimova <anastasiayy@yahoo.com>
2023-02-11 02:12:35 -0500

    SmashWords added (#36)

---
+++ src/test/java/anastasiayyTest/SmashWordsTest.java
@@ -0,0 +1,25 @@
+package anastasiayyTest;
+import static org.junit.Assert.*;
+
+import anastasiayy.SmashWords;
+import org.junit.Test;
+
+public class SmashWordsTest {
+
+    @Test
+    public void validate() {
+        assertEquals("Bilal Djaghout", SmashWords.smash(new String[] { "Bilal", "Djaghout" }));
+    }
+
+    @Test
+    public void validateEmpty() {
+        assertEquals("", SmashWords.smash(new String[] {}));
+    }
+
+
+    @Test
+    public void validateOneWord() {
+        assertEquals("Bilal", SmashWords.smash(new String[] {"Bilal"}));
+    }
+
+}
